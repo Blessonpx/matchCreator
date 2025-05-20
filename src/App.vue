d51100d65da3582d9a17e43fe4fd590fd6a87e88
Blessonpx <blessonxavierofficial@gmail.com>
2025-05-20 20:13:45 +0530

    Quill Launch

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h2>Manual Quill in Vue 3</h2>
+  <div id="editor" style="height: 800px;"></div>
+  <button @click="logContent">Log Content</button>
+</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import { onMounted } from 'vue'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+
+export default {
+  setup() {
+    let quillInstance = null;
+
+    onMounted(() => {
+      quillInstance = new Quill('#editor', {
+        theme: 'snow',
+        placeholder: 'Type something...',
+      });
+    });
+
+    function logContent() {
+      if (quillInstance) {
+        console.log('Quill content:', quillInstance.getText());
+      }
+    }
+
+    return { logContent }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+
+
+
+<style>
+#editor {
+  background: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Quill Settings
+import Vue3Quill from 'vue3-quill'
+import 'quill/dist/quill.snow.css'
+
+
+createApp(App)
+.use(Vue3Quill)
+.mount('#app')
